ae61ae563ce39ea56030b2612877c2d1eaa44048
Mateus Fortes <mateussilveirafortes@gmail.com>
2017-03-06 22:42:25 -0300

    Primeiro commit

---
+++ config/express.js
@@ -0,0 +1,33 @@
+// config/express.js
+var express = require('express');
+// var home = require('../app/routes/home');
+var load = require('express-load');
+var bodyParser = require('body-parser');
+
+module.exports = function() {
+	var app = express();
+
+	// configuração de ambiente
+	app.set('port', 3000);
+	
+	// middleware
+	app.use(express.static('./public'));
+
+	app.set('view engine', 'ejs');
+	app.set('views','./app/views');
+
+	// middleware
+	app.use(express.static('./public'));
+	app.use(bodyParser.urlencoded({extended: true}));
+	app.use(bodyParser.json());
+	app.use(require('method-override')());
+
+	// home(app);
+
+	load('models', {cwd: 'app'})
+		.then('controllers')
+		.then('routes')
+		.into(app);
+
+	return app;
+};
